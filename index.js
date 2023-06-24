3c00cd9a25d5da8300d6b9d7c6c67bb162bd78b9
shiva-saini <shivasainisiyana2211@gmail.com>
2023-06-24 18:16:01 +0530

    done

--- index.js
+++ index.js
@@ -6,12 +6,33 @@ let data_div = document.getElementById('data');
 let response = [];
 
 //function to load data initially
-function myFUnc(){
-    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+
+fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
     .then(response => response.json())
     .then((res) => {
         response = res.map((element) => {
-            let items = document.createElement('tr');
+             let ojb = {
+                image:element.image,
+                name:element.name,
+                symbol:element.symbol,
+                currentPrice:element.current_price,
+                marketCap:element.market_cap,
+                percentageChange:element.market_cap_change_percentage_24h
+             }
+             return ojb;
+        })
+    }).catch(err => {
+        console.log(err)
+})
+
+
+
+console.log(response)
+
+function myFUnc() {
+
+    response.map((element) => {
+    let items = document.createElement('tr');
             items.className = 'items'
             items.innerHTML = `
             <div class="item1">
@@ -35,28 +56,9 @@ function myFUnc(){
             </div>
                       `
     data_div.appendChild(items);
-    
-             let ojb = {
-                image:element.image,
-                name:element.name,
-                symbol:element.symbol,
-                currentPrice:element.current_price,
-                marketCap:element.market_cap,
-                percentageChange:element.market_cap_change_percentage_24h
-             }
-             return ojb;
-        })
-    }).catch(err => {
-        console.log(err)
     })
-
-
-
 }
 
-
-
-myFUnc();
 
 //here we are attaching addEventListener to the event to search values by name or symbol
 search_name_symbol.addEventListener('input',(event)=>{
@@ -195,5 +197,9 @@ sort_percentage.addEventListener('input',(event) => {
         data_div.appendChild(items);
 
     })
+
+   
 })
 
+
+myFUnc();
